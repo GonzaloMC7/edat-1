b41997296f5bdb9af0f734d4fc586fb8f37cb5f4
Adrián Fernández <adrian-fernandez-amador@users.noreply.github.com>
2016-11-30 21:31:55 +0100

    Objetivo 2

--- practica3/esecuele/development/database/record.c
+++ practica3/esecuele/development/database/record.c
@@ -1,21 +1,40 @@
 #include "record.h"
 
 struct record_ {
-  /* To be implemented */
+	int ncols;
+	void** values;
+	long next;
 };
 
 record_t* record_create(void** values, int ncols, long next) {
-  /* To be implemented */
+	record_t* record;
+
+	if( values == NULL || ncols < 0 || next < 0) return NULL;
+
+	record = malloc(sizeof(record_t));
+	if(record == NULL) return NULL;
+
+	record->ncols = ncols;
+	record->values = values;
+	record->next = next;
+
+	return record; 
 }
 
 void* record_get(record_t* record, int n) {
-  /* To be implemented */
+	if(record == NULL || n < 0) return NULL;
+
+	return record->values[n];
 }
 
 long record_next(record_t* record) {
-  /* To be implemented */
+	if(record == NULL) return -1;
+
+	return record->next;
 }
 
 void record_free(record_t* record) {
-  /* To be implemented */
+	free(record);
+
+	return;
 }
